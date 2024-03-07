877a06ae306da4167b352bc2c1391478a0628d71
chinhpd5 <chinhpd5@fpt.edu.vn>
2024-03-07 18:07:44 +0700

    post

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.use(express.json());
+
 const productList =[
   {id:1,name:"product1", price:300},
-  {id:2,name:"product2", price:300},
-  {id:3,name:"product3", price:300}
+  {id:2,name:"abc", price:300},
+  {id:3,name:"abc", price:300}
 ]
 
 app.get('/', (req, res) => {
@@ -15,8 +17,31 @@ app.get('/', (req, res) => {
 app.get('/product',(req,res)=>{
     const query = req.query;
     console.log(query);
+    //map, filter, reduce, some, every
+    const findProduct=  productList.filter(product => {
+      return product.name.includes(query.name)
+    } )
 
-    res.send(productList)
+    if(findProduct.length)
+      res.send(findProduct)
+    else
+      res.send(productList)
+})
+
+app.post('/product',(req,res)=>{
+  const data = req.body;
+  if(data){
+    productList.push(data)
+  }
+  res.send(productList);
+})
+
+app.put('/product/:id',(req,res)=>{
+  //
+})
+
+app.delete('/product/:id',(req,res)=>{
+  //
 })
 
 app.get('/product/:id',(req,res)=>{
