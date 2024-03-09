4dc3eb0f9d646e983318c83ac758c8cab860cead
chinhpd5 <chinhpd5@fpt.edu.vn>
2024-03-09 17:30:00 +0700

    update,delete

--- src/index.js
+++ src/index.js
@@ -28,22 +28,6 @@ app.get('/product',(req,res)=>{
       res.send(productList)
 })
 
-app.post('/product',(req,res)=>{
-  const data = req.body;
-  if(data){
-    productList.push(data)
-  }
-  res.send(productList);
-})
-
-app.put('/product/:id',(req,res)=>{
-  //
-})
-
-app.delete('/product/:id',(req,res)=>{
-  //
-})
-
 app.get('/product/:id',(req,res)=>{
   let id = req.params.id;
   const product = productList.find(item => item.id == id);
@@ -52,6 +36,60 @@ app.get('/product/:id',(req,res)=>{
   else
     res.send("Không tìm thấy sản phẩm")
 })
+
+app.post('/product',(req,res)=>{
+  const data = req.body;
+  if(data){
+    productList.push(data)
+  }
+  res.send(productList);
+})
+// sửa dữ liệu
+app.put('/product/:id',(req,res)=>{
+  //b1: lấy id
+  const id = req.params.id;
+  //b2: lấy data
+  const data = req.body;
+  //b3: kiểm tra data có tồn tại
+  if(data != {}){
+    // có dữ liệu
+    //b4: lấy index của sản phẩm theo id
+    if(id > 0){
+      const index = productList.findIndex(item=> item.id ==id);
+      if(index >= 0){
+        productList[index] = data;
+        res.send(productList);
+      }else{
+        res.send("không tìm thấy dữ liệu")
+      }
+    }
+  }else{
+    res.send("Không nhận được dữ liệu")
+  }
+  //kiểm index
+  // thay thế data mới vào data cũ
+  // gửi lại array productList
+})
+
+app.delete('/product/:id',(req,res)=>{
+  //b1: lấy id
+  const id = req.params.id;
+  // const isCheck = productList.some(item => item.id == id);
+  const index = productList.findIndex(item=> item.id ==id);
+
+  if(index>=0){
+    productList.splice(index,1);
+    res.send(productList)
+  }
+  else{
+    res.send('Không tìm thấy sản phẩm cần xóa')
+  }
+  //b2: kiểm tra id của sản phẩm đó có tồn tại hay k
+  //b3: xóa
+
+})
+
+
 
 app.get('/category',(req,res)=>{
   res.send('category')
