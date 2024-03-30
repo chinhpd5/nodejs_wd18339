4d70c29441ad6e7609cbb9b8211ba3db8f71ffd3
chinhpd5 <chinhpd5@fpt.edu.vn>
2024-03-30 16:46:37 +0700

    update cart

--- src/controllers/cart.controller.js
+++ src/controllers/cart.controller.js
@@ -3,7 +3,7 @@ import Cart from '../models/cart.model.js'
 //[POST] /cart
 export function addCart(req,res){
     const data = req.body;
-    console.log(data);
+    // console.log(data);
 
     if(!data && data == {})
         return res.status(400).json({message:"thiếu dữ liệu"});
@@ -13,7 +13,7 @@ export function addCart(req,res){
         .catch(err=> res.status(500).json({message: err}))
 
 }
-
+// [GET] cart/user/:id
 export function getCartByUser(req,res){
     const idUser = req.params.id;
     // console.log(userId);
@@ -27,4 +27,35 @@ export function getCartByUser(req,res){
         })
         .then(resData => res.json(resData))
         .catch(err=> res.json({message: err}))
+}
+
+//[PUT] cart/:id
+
+export async function updateItem(req,res){
+    try{
+        const id = req.params.id;
+        // console.log(id);
+        const cartNow = await Cart.findById(id);
+        // console.log(cartNow);
+        if(!cartNow || cartNow =={})
+            return res.status(400).json({message: "không tìm thấy giỏ hàng"});
+
+        const data = req.body;
+        // console.log(data);
+        if(!data || data=={}){
+            return res.status(400).json({message: "không nhận được dữ liệu"});
+        }
+        // gán danh sách sản phẩm mới vào sản phẩm cũ trong giỏ hàng
+        cartNow.items = data.items;
+        // lưu vào CSDL
+        await cartNow.save();
+
+        res.status(200).json({
+            message: "cập nhật thành công",
+            cartNow
+        })
+    }catch(err){
+        res.status(400).json({message: err})
+    }
+
 }
--- src/routers/cart.router.js
+++ src/routers/cart.router.js
@@ -1,11 +1,11 @@
 import express from 'express'
 const router = express.Router();
-import {addCart,getCartByUser} from '../controllers/cart.controller.js'
+import {addCart,getCartByUser,updateItem} from '../controllers/cart.controller.js'
 
 //Lấy danh sách
 // router.get('/',index);
 router.post('/',addCart)
 router.get('/user/:id',getCartByUser)
-
+router.put('/:id',updateItem)
 
 export default router;
